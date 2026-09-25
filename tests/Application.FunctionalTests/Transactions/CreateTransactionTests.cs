9e33282ac5a7c590fd6b7a65d36d7738f8418b02
louis <16326681+louis297@users.noreply.github.com~>
2026-09-26 01:06:36 +1200

    Add failing test for customer transaction access.

--- tests/Application.FunctionalTests/Transactions/CreateTransactionTests.cs
+++ tests/Application.FunctionalTests/Transactions/CreateTransactionTests.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using MyWealthV2.Domain.Enums;
 
 namespace MyWealthV2.Application.FunctionalTests.Transactions;
 
@@ -20,5 +21,33 @@ public class CreateTransactionTests : TestBase
         post.Headers.Location.ShouldBeNull();
         get.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
         get.Headers.Location.ShouldBeNull();
+    }
+
+    [Test]
+    public async Task Customer_Returns403()
+    {
+        var tenant = await TestApp.CreateTenantAsync("Firm C", "firmc");
+        var adviser = await TestApp.CreatePersonAsync(
+            UserRole.Adviser, "Cara", "cara@firmc", "Password1!", tenant.Id);
+        var customer = await TestApp.CreatePersonAsync(
+            UserRole.Customer, "Dee", "dee@firmc", "Password1!", tenant.Id, adviser.Id);
+        var tokens = await TestApp.IssueAccessTokenAsync(customer);
+        var body = new
+        {
+            accountId = Guid.NewGuid(),
+            type = "TransferIn",
+            amount = 100.00m,
+            bookedAt = "2026-09-24T09:00:00+12:00"
+        };
+
+        var post = await TransactionHttp.Send(
+            HttpMethod.Post, "/transactions", tokens.AccessToken, body, Guid.NewGuid());
+        var list = await TransactionHttp.Send(HttpMethod.Get, "/transactions", tokens.AccessToken);
+        var item = await TransactionHttp.Send(
+            HttpMethod.Get, $"/transactions/{Guid.NewGuid()}", tokens.AccessToken);
+
+        post.StatusCode.ShouldBe(HttpStatusCode.Forbidden);
+        list.StatusCode.ShouldBe(HttpStatusCode.Forbidden);
+        item.StatusCode.ShouldBe(HttpStatusCode.Forbidden);
     }
 }
